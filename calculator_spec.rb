c9f05e76ab361a72dee642dd8fddda7d5bb460bc
Roberto VP <roberto.valladarespiedras@gmail.com>
2016-04-06 10:38:56 -0700

    This is the subtract branch

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,1 +1,36 @@
 require_relative 'calculator'
+
+RSpec.describe Calculator do
+
+	before do
+		@subtractor = Calculator.new
+	end
+
+	it "subtracts all numbers in an array from index zero" do
+
+		@subtractor.subtract(60,20)
+		expect(@subtractor.result).to eq(40)
+	end
+
+	it "raises an error when you enter a string" do
+		expect{@subtractor.subtract('hola')}.to raise_error
+	end
+
+	it "subtracts from zero when you only enter one parameter" do
+
+		@subtractor.subtract(20)
+		expect(@subtractor.result).to eq(-20)
+	end
+
+	it "can chain several subtractions" do
+
+		@subtractor.subtract(100,10).subtract(5)
+		expect(@subtractor.result).to eq(85)
+	end
+
+	it "subtracts fractions" do
+		@subtractor.subtract(10,3.47)
+		expect(@subtractor.result).to eq(6.53)
+	end
+
+end
